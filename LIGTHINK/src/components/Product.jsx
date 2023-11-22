45535898ad40251cf962173e41b00ca362c7258e
Kuba <93491047+K0ntek@users.noreply.github.com>
2023-11-22 12:01:13 +0100

    Product.jsx

--- LIGTHINK/src/components/Product.jsx
+++ LIGTHINK/src/components/Product.jsx
@@ -256,16 +256,16 @@ const Product = () => {
 				</div>
 
 				<div className="grid lg:grid-cols-2 text-white w-[100%] mx-auto gap-5 mt-8 relative lg:top-[20px] ">
-						<div className='p-6 bg-[silver] text-black rounded-3xl z-[5]'>
+						<div className='w-[95%] mx-auto p-6 bg-[#0e0e0e] rounded-3xl z-[5]'>
 							<h1 className=' text-3xl font-megrim ml-[10%]'>SKLAD</h1>
 							<p className='text-justify mt-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quasi velit ipsum aliquid dolorum dolorem, non modi doloribus accusamus aspernatur necessitatibus saepe, natus explicabo eius quia beatae amet consectetur sit a illo fuga nulla provident neque. Aperiam blanditiis, quam ab vitae ea asperiores magni ipsam vel! Qui dolores id ex.</p>
 						</div>
-						<div className='p-6 bg-[#12411e] rounded-3xl relative md:left-[50px] lg:left-0 lg:top-[50%] z-[5]'>
+						<div className=w-[95%] mx-auto 'p-6 bg-[#07180c] rounded-3xl relative md:left-[50px] lg:left-0 lg:top-[50%] z-[5]'>
 							<h1 className=' text-3xl font-megrim ml-[10%]'>O PRODUKCIE</h1>
 							<p className='text-justify mt-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit rem, mollitia voluptatum cupiditate obcaecati maxime voluptates. Quaerat reprehenderit quas alias consequatur eum libero dolores voluptatem quod aperiam? Commodi libero amet nulla tempore accusantium quidem sequi pariatur reprehenderit maiores quisquam ducimus, nostrum, harum alias ratione impedit exercitationem soluta veniam temporibus aliquam.</p>
 						</div>
 
-						<div className='p-6 bg-[#030303] shadow-[-32px_36px_40px_-26px] shadow-white rounded-3xl relative md:left-[-40px] z-[5]'>
+						<div className=w-[95%] mx-auto 'p-6 bg-[#030303] shadow-[-20px_20px_37px_-26px] shadow-white rounded-3xl relative md:left-[-40px] z-[5]'>
 							<h1 className=' text-3xl font-megrim ml-[10%]'>DOSTAWA</h1>
 							<p className='text-justify mt-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit rem, mollitia voluptatum cupiditate obcaecati maxime voluptates. Quaerat reprehenderit quas alias consequatur eum libero dolores voluptatem quod aperiam? Commodi libero amet nulla tempore accusantium quidem sequi pariatur reprehenderit maiores quisquam ducimus, nostrum, harum alias ratione impedit exercitationem soluta veniam temporibus aliquam.</p>
 						</div>
