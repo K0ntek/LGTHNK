49ad060d56d7aa4bda31669bdd459774a446f9b7
Kuba <93491047+K0ntek@users.noreply.github.com>
2024-01-22 21:45:00 +0100

    Update Product.jsx

--- LIGTHINK/src/components/Product.jsx
+++ LIGTHINK/src/components/Product.jsx
@@ -183,7 +183,7 @@ const Product = () => {
 					<h1 className=" font-poppins font-extrabold text-4xl">{product.title}</h1>
 					<p className="mt-6 ml-4 font-questrial text-2xl">{product.price * counter} PLN</p>
 
-					<div className=" mt-6">
+					{/* <div className=" mt-6">
 						<p className="font-poppins text-sm ml-5">LICZBA SZTUK</p>
 						<div className="flex space-x-2 w-fit">
 							<button className=" aspect-square w-8 m-auto text-lg rounded-full border-[1px] border-gray-300 bg-gray-100 hover:bg-gray-300 transition-all duration-200" onClick={decrease}>
@@ -194,7 +194,7 @@ const Product = () => {
 								<AiOutlinePlus className='text-center mx-auto'/>
 							</button>
 						</div>
-					</div>
+					</div> */}
 
 					<div className="mt-6 mb-4">
 						{/* <h1 className=" font-questrial font-bold text-xl ml-3">ROZMIAR</h1> */}
@@ -224,15 +224,15 @@ const Product = () => {
 
 					<div>
 
-						<div className='grid grid-cols-5 w-[250px] my-3'>
+						<div className='grid grid-cols-5 w-[220px] my-3'>
 						{product["colors"]?.map((element)=>{
 									return(
 										<div className='group p-1' onClick={() => changeActiveColor(element.id)}>
-											<p className=' text-[10px] text-center'>{element.colorName}</p>
+											{/* <p className=' text-[12px] text-center font-poppins'>{element.colorName}</p> */}
 											<div className=
 											{`${
 												activeColor == element.id ? 'activeColor' : ''
-											} color  w-full rounded-lg aspect-[1/1] relative overflow-hidden hover:rounded-2xl transition-all duration-200`} style={{background: `${element.color}`}}>
+											} color  w-full rounded-lg aspect-[1/1] relative overflow-hidden`} style={{background: `${element.color}`}}>
 												{/* <div className=' absolute top-0 left-0 w-full h-full group-hover:bg-gradient-to-t from-black/30 to-transparent'>
 													<div><p className=' absolute bottom-[-20px] left-[50%] translate-x-[-50%] font-questrial text-sm text-white group-hover:bottom-[30px] transition-all duration-200'>{element.colorName}</p></div>
 												</div> */}
@@ -257,7 +257,7 @@ const Product = () => {
 		</div>
 
 
-		<div className="w-full min-h-fit bg-[#000000] mt-[10%] relative">
+		<div className="w-full min-h-fit bg-[#000000] mt-[10%] relative pb-9">
 			<div className="md:w-[70%] bg-[#020202] rounded-3xl h-fit relative md:left-[15%] md:top-[-30px] sm:top-[-50px] top-[-30px]">
 
 				<div className="relative w-full">
@@ -349,7 +349,7 @@ const Product = () => {
 				</div>
 
 				<div className="grid lg:grid-cols-2 text-white w-[100%] mx-auto gap-5 mt-8 relative lg:top-[20px] ">
-						<div className='w-[95%] mx-auto p-6 bg-[#020202] text-[silver] rounded-3xl z-[5] shadow-2xl shadow-gray-900'>
+						<div className='w-[95%] mx-auto p-6 bg-[#020202] text-[silver] rounded-3xl z-[5] shadow-lg shadow-gray-950'>
 							<h1 className=' text-3xl font-megrim ml-[10%] font-bold'>SKLAD</h1>
 							<div className="font-questrial">
 									<div>
@@ -365,12 +365,12 @@ const Product = () => {
 									</div>
 							</div>
 						</div>
-						<div className='w-[95%] mx-auto p-6 bg-[#DAE3E5] text-[#88502b] rounded-3xl relative md:left-[50px] lg:left-0 lg:top-[60px] z-[5] shadow-xl shadow-gray-800'>
+						<div className='w-[95%] mx-auto p-6 bg-[#DAE3E5] text-[#88502b] rounded-3xl relative md:left-[50px] lg:left-0 lg:top-[60px] z-[5] shadow-lg shadow-[#4b3526]'>
 							<h1 className=' text-3xl font-megrim ml-[10%] font-bold'>O PRODUKCIE</h1>
 							<p className='text-justify mt-4 font-questrial text-xl' >{product.description}</p>
 						</div>
 
-						<div className='w-[95%] mx-auto p-6 bg-[#060913] text-[silver] rounded-3xl relative lg:left-[40px] z-[5] shadow-lg shadow-gray-950'>
+						<div className='w-[95%] mx-auto p-6 bg-[#060913] text-[silver] rounded-3xl relative lg:left-[40px] z-[5] shadow-lg shadow-[#0a0908]'>
 							<h1 className=' text-3xl font-megrim ml-[10%] font-bold'>DOSTAWA</h1>
 								<div className=" font-questrial text-xl">
 									<p className='text-justify mt-4'>Dbamy o to, aby Twoja bluza dotarła do Ciebie szybko, bezpiecznie i z pełnym zadowoleniem. Oto szczegóły dotyczące dostawy:</p>
