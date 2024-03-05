dc3378a3285e7bd5a735169fffc5b54306a07828
Kuba <93491047+K0ntek@users.noreply.github.com>
2024-03-05 12:13:03 +0100

    Update CategoryWrapper.jsx

--- LIGTHINK/src/components/CategoryWrapper.jsx
+++ LIGTHINK/src/components/CategoryWrapper.jsx
@@ -67,7 +67,7 @@ const CategoryWrapper =({header, content, categoriesBackgrounds, backgroundImage
         <div className=" mx-auto relative top-[30%] space-y-[10%]">
           <div className=" space-y-10">
             <h1 className="header font-inter text-[#d4d4d4] text-5xl sm:text-7xl tracking-[5px] productsTitle text-center">{header}</h1>
-            <div className=" w-1/3 mx-auto text-justify mt-5 h-[100px]">
+            <div className=" md:w-1/3 mx-auto text-justify mt-5 h-[100px]">
             <TypeAnimation
               sequence={[' ', 200, `${content}`]}
               speed={80}
@@ -80,7 +80,7 @@ const CategoryWrapper =({header, content, categoriesBackgrounds, backgroundImage
           <div className=" justify-center mx-auto text-center space-y-8">
             {/* <h1 className=" text-white text-3xl font-michroma">KOLEKCJE</h1> */}
 
-            <div className=" flex gap-3 justify-center flex-wrap buttons w-4/5 mx-auto">
+            <div className=" flex gap-3 justify-center flex-wrap buttons sm:w-4/5 mx-auto">
               {buttons.map((button, i) => <CategoryButtons button={button} />)}
             </div>
           </div>
